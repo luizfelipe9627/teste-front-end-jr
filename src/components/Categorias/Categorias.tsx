cfba44f214bd009ab19a3c52fdfa03bb8351f3bd
Luiz Felipe Silva <felipesilva9627@hotmail.com>
2023-09-16 03:41:36 -0300

    chore: alterado nome de componente

--- src/components/Categorias/Categorias.tsx
+++ src/components/Categorias/Categorias.tsx
@@ -10,20 +10,23 @@ import moda from "../../assets/img/moda.png";
 import mercado from "../../assets/img/mercado.png";
 import padaria from "../../assets/img/padaria.png";
 import oriental from "../../assets/img/oriental.png";
+import CarouselCategorias from "./CarouselCategorias";
 
 export default function Categorias() {
   return (
     <section className={styles.categorias}>
-      <CategoriaData img={tecnologia} title="Tecnologia" active={true} />
-      <CategoriaData img={supermercado} title="Supermercado" />
-      <CategoriaData img={bebida} title="Bebidas" />
-      <CategoriaData img={ferramenta} title="Ferramentas" />
-      <CategoriaData img={saude} title="Saúde" />
-      <CategoriaData img={corrida} title="Esportes e Fitness" />
-      <CategoriaData img={moda} title="Moda" />
-      <CategoriaData img={mercado} title="Mercado" />
-      <CategoriaData img={padaria} title="Padaria" />
-      <CategoriaData img={oriental} title="Oriental" />
+      <CarouselCategorias>
+        <CategoriaData img={tecnologia} title="Tecnologia" active={true} />
+        <CategoriaData img={supermercado} title="Supermercado" />
+        <CategoriaData img={bebida} title="Bebidas" />
+        <CategoriaData img={ferramenta} title="Ferramentas" />
+        <CategoriaData img={saude} title="Saúde" />
+        <CategoriaData img={corrida} title="Esportes e Fitness" />
+        <CategoriaData img={moda} title="Moda" />
+        <CategoriaData img={mercado} title="Mercado" />
+        <CategoriaData img={padaria} title="Padaria" />
+        <CategoriaData img={oriental} title="Oriental" />
+      </CarouselCategorias>
     </section>
   );
 }
