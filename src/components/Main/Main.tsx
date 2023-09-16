01349663022341dd7728faf29d32a72f2a0296aa
Luiz Felipe Silva <felipesilva9627@hotmail.com>
2023-09-16 00:40:26 -0300

    feat: adicionado o import do componente parceiros

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,7 +1,7 @@
 import Banner from "../Banner/Banner";
 import Categorias from "../Categorias/Categorias";
+import Parceiros from "../Parceiros/Parceiros";
 import Vitrine from "../Vitrine/Vitrine";
-
 
 export default function Main() {
   return (
@@ -9,6 +9,7 @@ export default function Main() {
       <Banner />
       <Categorias />
       <Vitrine />
+      <Parceiros />
     </main>
   );
 }
