e136adb41f2d71a96b2dd23ccfee9ab7f491315e
Luiz Felipe Silva <felipesilva9627@hotmail.com>
2023-09-16 01:20:25 -0300

    chore: passado o codigo do TitleData para o Title

--- src/components/Title/Title.tsx
+++ src/components/Title/Title.tsx
@@ -1,5 +1,26 @@
-import TitleData from "./TitleData";
+import styles from "./Title.module.scss";
 
-export default function Title() {
-  return <TitleData title="Produtos relacionados" />;
+interface TitleProps {
+  title: string;
+  titleLink?: string;
+  href?: string;
+  lines?: boolean;
+}
+
+export default function Title(props: TitleProps) {
+  return (
+    <div className={styles.title}>
+      <div className={styles.text}>
+        {props.lines && <span></span>}
+        <h1>{props.title}</h1>
+        {props.lines && <span></span>}
+      </div>
+
+      {props.titleLink && (
+        <div className={styles.link}>
+          <a href={props.href}>{props.titleLink}</a>
+        </div>
+      )}
+    </div>
+  );
 }
