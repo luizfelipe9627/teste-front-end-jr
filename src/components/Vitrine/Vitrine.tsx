6c6f97b409bb4c3a9cec873debccb36d950e9531
Luiz Felipe Silva <felipesilva9627@hotmail.com>
2023-09-15 23:26:23 -0300

    feat: criado componente que mostra a vitrine

---
+++ src/components/Vitrine/Vitrine.tsx
@@ -0,0 +1,13 @@
+import Card from "../Card/Card";
+import Nav from "../Nav/Nav";
+import TitleData from "../Title/TitleData";
+
+export default function Vitrine() {
+  return (
+    <section>
+      <TitleData title="Produtos relacionados" />
+      <Nav />
+      <Card />
+    </section>
+  );
+}
