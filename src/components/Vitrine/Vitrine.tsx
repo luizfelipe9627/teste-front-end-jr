5ec2f049f4508d796c356d2a01f59d70826220b5
Luiz Felipe Silva <felipesilva9627@hotmail.com>
2023-09-16 01:21:43 -0300

    chore: alterado nome do componente Title

--- src/components/Vitrine/Vitrine.tsx
+++ src/components/Vitrine/Vitrine.tsx
@@ -1,11 +1,11 @@
 import Card from "../Card/Card";
 import Nav from "../Nav/Nav";
-import TitleData from "../Title/TitleData";
+import TitleData from "../Title/Title";
 
 export default function Vitrine() {
   return (
     <section>
-      <TitleData title="Produtos relacionados" />
+      <TitleData title="Produtos relacionados" lines/>
       <Nav />
       <Card />
     </section>
